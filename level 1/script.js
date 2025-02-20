aa339e45129bee79e10a4fba2ca781ca64fc5a32
Athira2499 <cathira899@gmail.com>
2025-02-20 14:55:49 +0530

    master

---
+++ level 1/script.js
@@ -0,0 +1,92 @@
+//setup and initialization
+document.addEventListener("DOMContentLoaded", function(){
+    const inputScreen = document.getElementById("input-screen");
+    const displayField = document.getElementById("display");
+    const buttons = document.querySelectorAll("button");
+    let expression = "";
+    let shouldClearInput = false;
+
+    //clear input screen
+    function clearInputScreen(){
+        expression = "";
+        inputScreen.value = "";
+        displayField.value = "";
+        shouldClearInput = false;
+    }
+    //detele the last character or backspace
+    function deleteLastCharacter(){
+        expression = expression.slice(0,-1);
+        inputScreen.value = expression;
+    }
+    //to evaluate expression, to give result
+    function evaluateExpression(){
+        try{
+            const result = eval(expression).toString();//eval(2+2).toString
+            displayField.value = expression;//2+2
+            inputScreen.value = result;//4
+            shouldClearInput = true;
+            expression = "";
+        }
+        catch(error){
+            inputScreen.value = "Error";
+        }
+    }
+    //for operation 
+    function addOperator(operator){
+        if(shouldClearInput){
+            clearInputScreen();
+        }
+        expression = expression + operator;
+        inputScreen.value = expression;
+    }
+    //fn to append a number or dot to the expression
+    function appendToExpression(text){
+        if(shouldClearInput){
+            clearInputScreen();
+        }
+        expression = expression + text;
+        inputScreen.value = expression;
+    }
+
+    //add the event listener to each button
+    buttons.forEach(function(button){
+        button.addEventListener("click",function(){
+            if(button.classList.contains("clear-icon")){
+                clearInputScreen();
+                console.log("clicked");
+            }
+            else if(button.classList.contains("delete-icon")){
+                deleteLastCharacter();
+                console.log("clicked");
+            }
+            else if(button.classList.contains("equal-icon")){
+                evaluateExpression();
+                console.log("clicked");
+            }
+            else if(button.classList.contains("divide-icon")){
+                addOperator("/");
+                console.log("clicked");
+            }
+            else if(button.classList.contains("multiply-icon")){
+                addOperator("*");
+                console.log("clicked");
+            }
+            else if(button.classList.contains("add-icon")){
+                addOperator("+");
+                console.log("clicked");
+            }
+            else if(button.classList.contains("minus-icon")){
+                addOperator("-");
+                console.log("clicked");
+            }
+            else if(button.classList.contains("precentage-icon")){
+                addOperator("%");
+                console.log("clicked");
+            }
+            else{
+                appendToExpression(button.innerText);//concantenate or append :234
+                console.log("clicked");
+            }
+        });
+    });
+});
